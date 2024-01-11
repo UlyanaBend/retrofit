69f4cdc8f787f8e5791010201c29ee092d4b3384
Ulyana <ulanabend1@gmail.com>
2024-01-11 20:05:19 +0200

    addAuth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,16 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("com.squareup.picasso:picasso:2.8")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.retrofit
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.example.retrofit.databinding.ActivityMainBinding
+import com.example.retrofit.retrofit.AuthRequest
+import com.example.retrofit.retrofit.MainAPI
 import com.example.retrofit.retrofit.ProductAPI
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,14 +17,18 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://dummyjson.com")
             .addConverterFactory(GsonConverterFactory.create()).build()
         val productAPI = retrofit.create(ProductAPI::class.java)
+        val mainAPI = retrofit.create(MainAPI::class.java)
 
         val tv = findViewById<TextView>(R.id.tv)
         val b = findViewById<Button>(R.id.button)
@@ -29,6 +38,24 @@ class MainActivity : AppCompatActivity() {
                 val product = productAPI.getProductById(2)
                 runOnUiThread {
                     tv.text = product.title
+                }
+            }
+        }
+
+        binding.signin.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val user = mainAPI.auth(
+                    AuthRequest(
+                        binding.username.text.toString(),
+                        binding.password.text.toString()
+                    )
+                )
+                runOnUiThread {
+                    binding.apply {
+                        Picasso.get().load(user.image).into(iv)
+                        firstName.text = user.firstName
+                        lastName.text = user.lastName
+                    }
                 }
             }
         }
