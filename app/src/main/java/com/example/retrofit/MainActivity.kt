e24213d7a8621f820899b52a33390d3ca4a154b2
Ulyana <ulanabend1@gmail.com>
2024-01-11 15:28:22 +0200

    SAT-423/feature-addretrofit

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.retrofit.retrofit.ProductAPI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        val productAPI = retrofit.create(ProductAPI::class.java)
+
+        val tv = findViewById<TextView>(R.id.tv)
+        val b = findViewById<Button>(R.id.button)
+
+        b.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val product = productAPI.getProductById(2)
+                runOnUiThread {
+                    tv.text = product.title
+                }
+            }
+        }
     }
 }
